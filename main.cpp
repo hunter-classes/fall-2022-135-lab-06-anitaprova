92dadab4956902d6c3464e2f1a5d70a7f8af379a
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-09 22:08:37 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
-
-#include "funcs.h"
+#include "test-ascii.h"
+#include "caesar.h"
 
 int main()
 {
-  return 0;
+  	test_ascii("Cat :3 Dog");
+	std::cout << shiftChar('W', 10) << std::endl;
+	std::cout << encryptCaesar("Hello, World!", 10) << std::endl;	
+	
+	return 0;
 }
