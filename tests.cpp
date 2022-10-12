4d885151bd1efd2c2b1206f3ec8556740a963015
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-11 21:48:18 -0400

    added more tests

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "test-ascii.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
-// add your tests here
+TEST_CASE("caesar"){
+	CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+	CHECK(encryptCaesar("Hello, World!", 10) == "Rovvy, Gybvn!");
+}
+
+TEST_CASE("vigenere"){
+	CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
+}
+
+TEST_CASE("decrypt"){
+	CHECK(decryptCaesar("Bfd yt Lt!", 5) == "Way to Go!");
+	CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
+
+	CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
+}
+
