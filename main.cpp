5777571f883d11aa64e6c0c79930741129528e1a
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-11 22:12:01 -0400

    removed test-ascii

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include "test-ascii.h"
 #include "caesar.h"
 #include "vigenere.h"
 #include "decrypt.h"
 
 int main()
 {
-  	test_ascii("Cat :3 Dog");
-	
 	std::cout << shiftChar('W', 10) << std::endl;
 	
 	std::cout << encryptCaesar("Hello, World!", 10) << std::endl;	
