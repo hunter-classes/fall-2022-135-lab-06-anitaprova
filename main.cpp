bb2ca54071a7ed821d639650d498415226534c94
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-11 21:25:50 -0400

    added functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "test-ascii.h"
 #include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 int main()
 {
   	test_ascii("Cat :3 Dog");
+	
 	std::cout << shiftChar('W', 10) << std::endl;
+	
 	std::cout << encryptCaesar("Hello, World!", 10) << std::endl;	
 	
+	std::cout << encryptVigenere("Hello, World!", "cake") << std::endl;
+	
+	std::cout << decryptCaesar("Rovvy, Gybvn!", 10) << std::endl;
+	
+	std::cout << decryptVigenere("Jevpq, Wyvnd!", "cake") << std::endl;
+
 	return 0;
 }
