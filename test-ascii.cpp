4e55d85725578d3bf1814aa1c4dcb93a35e1ee42
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-13 20:55:54 -0400

    removed function

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-std::string test_ascii(std::string str){
-	for(int i = 0; i < str.size(); i++){
+int main(){
+	std::string str;
+	getline(std::cin, str);
+	
+	for (int i = 0; i < str.size(); i++){
 		std::cout << str[i] << " " << (int)str[i] << std::endl;
 	}
-	return "";
-}
 
-int main(){
-	test_ascii("Cat :3 Dog");
+	return 0;
 }
